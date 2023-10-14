d7f332e96d88579f1da680e77fd32bd1dd7ad01d
LeCua <ultrav8800@gmail.com>
2023-10-14 10:23:15 +0700

    a

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -20,7 +20,8 @@ class AdHelper {
     if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      // return 'ca-app-pub-3940256099942544/2934735716';
+      return "ca-app-pub-9396261057575895/9145693478";
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -30,7 +31,8 @@ class AdHelper {
     if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/7049598008';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/3964253750';
+      // return 'ca-app-pub-3940256099942544/3964253750';
+      return 'ca-app-pub-9396261057575895/3172926464';      
     } else {
       throw UnsupportedError('Unsupported platform');
     }
